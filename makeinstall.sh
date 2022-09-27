b3e3e1c2a113f03cd5feaf2770c629d8de1e355b
Zeckma <83990963+Zeckmathederg@users.noreply.github.com>
2022-09-27 17:05:54 +0000

    Updated makeinstall.sh
    Changed naming schemes from the mntdrive install script to better match transx's theme.

--- makeinstall.sh
+++ makeinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This file is meant to compile main.cpp and install the executable to the install path
-export mntdriveinstallpath=/usr/local/bin
+export transxinstallpath=/usr/local/bin
 # Change the line above to change install directory. The default is /usr/local/bin. Make sure it is in your PATH.
-g++ -o ./mntdrive main.cpp && mv -v mntdrive $mntdriveinstallpath
-echo "The package mntdrive has successfully been installed"
-# Run mntdrive to make sure it works!
-unset $mntdriveinstallpath
+g++ -o ./transx main.cpp && mv -v transx $transxinstallpath
+echo "The package transx has successfully been installed"
+# Run transx to make sure it works!
+unset $transxinstallpath
